d00b47dd9673dcd6845c295598707d52f13e3936
unifusions <siyam@unifusions.com>
2026-09-01 22:53:24 +0530

    vite changes

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -29,7 +29,7 @@ createInertiaApp({
         }
     },
     strictMode: true,
-    withApp(app) {
+    withApp(app) { 
         return (
             <TooltipProvider delayDuration={0}>
             <BreadcrumbProvider >
--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,7 @@ export default defineConfig({
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.tsx'],
             refresh: true,
-            buildDirectory: 'build', 
+           
              
         }),
         inertia(),
